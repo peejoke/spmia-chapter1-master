cbb183c688056fa2501b7e13982adfecedd6d0ac
peejoke <peejoke@yahoo.com>
2020-10-29 12:01:47 +0700

    Initial commit

--- simpleservice/src/main/docker/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "********************************************************"
-echo "Starting simple-service "
-echo "********************************************************"
-java -jar /usr/local/simple-service/@project.build.finalName@.jar
